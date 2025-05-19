0dba603cedcbede5273137ca5ad5e6407be0a2c3
ekremarmagankarakas <ekremarmagankarakas@gmail.com>
2025-05-19 03:16:02 +0300

    general test fix for new lexer

--- tests/general_test.py
+++ tests/general_test.py
@@ -5,9 +5,12 @@ code = "fun greet(x) { yell(x + 2); }"
 
 functioncall = "fun main() { greet(7); }"
 
-lexer = Lexer(code)
-tokens = lexer.tokenizer()
-print("Tokens:", tokens)
+lexer = Lexer("<stdin>", code)
+tokens, err = lexer.tokenizer()
+if err:
+  print(err.as_string())
+else:
+  print("Tokens:", tokens)
 
 parser = Parser(tokens)
 program = parser.parse()
