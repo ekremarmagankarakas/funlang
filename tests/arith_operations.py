707914c272c5357af94557886c719a8326bbfad4
ekremarmagankarakas <ekremarmagankarakas@gmail.com>
2025-06-07 00:19:38 +0300

    arith operations tests and base test added

---
+++ tests/arith_operations.py
@@ -0,0 +1,67 @@
+from tests.test_base import test
+
+addition_test = "4 + 3"
+assert test(addition_test).value == 7
+
+remove_test = "5 - 2"
+assert test(remove_test).value == 3
+
+multiply_test = "3 * 5"
+assert test(multiply_test).value == 15
+
+divide_test = "21 / 3"
+assert test(divide_test).value == 7
+
+powered_test = "3 ^ 2"
+assert test(powered_test).value == 9
+
+comparison_equals_test = "3 == 3"
+comparison_equals_test2 = "3 == 4"
+assert test(comparison_equals_test).value == 1
+assert test(comparison_equals_test2).value == 0
+
+comparison_not_equals_test = "3 != 4"
+comparison_not_equals_test2 = "3 != 3"
+assert test(comparison_not_equals_test).value == 1
+assert test(comparison_not_equals_test2).value == 0
+
+comparison_less_than_test = "3 < 4"
+comparison_less_than_test2 = "3 < 3"
+comparison_less_than_test3 = "3 < 2"
+assert test(comparison_less_than_test).value == 1
+assert test(comparison_less_than_test2).value == 0
+assert test(comparison_less_than_test3).value == 0
+
+comparison_greater_than_test = "4 > 3"
+comparison_greater_than_test2 = "4 > 4"
+comparison_greater_than_test3 = "4 > 5"
+assert test(comparison_greater_than_test).value == 1
+assert test(comparison_greater_than_test2).value == 0
+assert test(comparison_greater_than_test3).value == 0
+
+comparison_less_than_or_equals_test = "3 <= 4"
+comparison_less_than_or_equals_test2 = "3 <= 3"
+comparison_less_than_or_equals_test3 = "3 <= 2"
+assert test(comparison_less_than_or_equals_test).value == 1
+assert test(comparison_less_than_or_equals_test2).value == 1
+assert test(comparison_less_than_or_equals_test3).value == 0
+
+comparison_greater_than_or_equals_test = "3 >= 2"
+comparison_greater_than_or_equals_test2 = "3 >= 3"
+comparison_greater_than_or_equals_test3 = "3 >= 4"
+assert test(comparison_greater_than_or_equals_test).value == 1
+assert test(comparison_greater_than_or_equals_test2).value == 1
+assert test(comparison_greater_than_or_equals_test3).value == 0
+
+anded_with_test = "2 and 3"
+assert test(anded_with_test).value == 3
+
+ored_with_test = "0 or 4"
+assert test(ored_with_test).value == 4
+
+notted_test = "not 0"
+notted_test2 = "not 1"
+notted_test3 = "not 2"
+assert test(notted_test).value == 1
+assert test(notted_test2).value == 0
+assert test(notted_test3).value == 0
---
+++ tests/test_base.py
@@ -0,0 +1,32 @@
+from lexer import Lexer
+from parser import Parser
+from interpreter import Interpreter, Context, SymbolTable, Number
+
+global_symbol_table = SymbolTable()
+global_symbol_table.set("null", Number(0))
+global_symbol_table.set("false", Number(0))
+global_symbol_table.set("true", Number(1))
+
+
+def test(source):
+  file_name = '<stdin>'
+  lexer = Lexer(file_name, source)
+  tokens, error = lexer.tokenizer()
+  if error:
+    return False
+
+  parser = Parser(tokens)
+  ast = parser.parse()
+  if ast.error:
+    return False
+
+  interpreter = Interpreter()
+  context = Context("<program>")
+  context.symbol_table = global_symbol_table
+  result = interpreter.visit(ast.node, context)
+
+  if result.error:
+    return False
+
+  return result.value
+
