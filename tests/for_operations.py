de33852a26e4965bd2f437ee16b30d58b0adb8bb
ekremarmagankarakas <ekremarmagankarakas@gmail.com>
2025-06-11 02:25:14 +0300

    test fix

--- tests/for_operations.py
+++ tests/for_operations.py
@@ -1,7 +1,4 @@
 from tests.test_base import test
 
-for_test = "var j = 0; for i=0, 6 { var j = i; }; j"
-#assert test(for_test).elements[0].value == 5
-
-for_test2 = "for i=0, 6 { var j = i; }; j"
-#assert test(for_test2).elements[0].value == 5
+for_test = "for i=0, 6 { var j = i; }; j"
+assert test(for_test).elements[-1].value == 5
--- tests/runall.py
+++ tests/runall.py
@@ -2,3 +2,4 @@ import tests.list_operations
 import tests.arith_operations
 import tests.if_operations
 import tests.for_operations
+import tests.function_operations
