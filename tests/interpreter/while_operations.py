760bcc602649fe6d4b0ced7d1ad17385f34a0475
ekremarmagankarakas <ekremarmagankarakas@gmail.com>
2025-06-15 03:06:52 +0300

    interpreter break and continue test added

--- tests/interpreter/while_operations.py
+++ tests/interpreter/while_operations.py
@@ -1,7 +1,14 @@
 from tests.interpreter.test_base import test
 
 while_test = "var j = 0; var i = 0; while i!=6 { j = i; i = i + 1 }; j;"
+while_break_test = "var j = 0; var i = 0; while i!=6 { if i == 3 { break; }; j = i; i = i + 1 }; j;"
+while_continue_test = "var j = 0; var i = 0; while i!=6 { i = i + 1; if i == 3 { continue; }; j = i; }; j;"
 
 while_test_result = test(while_test)
 assert while_test_result.elements[-1].value == 5
 
+while_break_test_result = test(while_break_test)
+assert while_break_test_result.elements[-1].value == 2
+
+while_continue_test_result = test(while_continue_test)
+assert while_continue_test_result.elements[-1].value == 6
